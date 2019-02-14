375aa4be0128a06c7ffacf1f69c1f1586f9495e1
Beverly Pham <beverly.pham@gmail.com>
2019-02-14 15:48:22 -0800

    add entry widgets to receive input

--- bookstore/frontend.py
+++ bookstore/frontend.py
@@ -17,4 +17,20 @@ l1.grid(row=1, column=0)
 l1 = Label(window, text='ISBN')
 l1.grid(row=1, column=2)
 
+title = StringVar()
+e1 = Entry(window, textvariable=title)
+e1.grid(row=0, column=1)
+
+author = StringVar()
+e2 = Entry(window, textvariable=author)
+e2.grid(row=0, column=3)
+
+year = StringVar()
+e3 = Entry(window, textvariable=year)
+e3.grid(row=1, column=1)
+
+isbn = StringVar()
+e4 = Entry(window, textvariable=isbn)
+e4.grid(row=1, column=3)
+
 window.mainloop()

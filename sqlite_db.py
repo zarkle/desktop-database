d7ea623f9fd69ac65602290c690d5cfcce23bd15
Beverly Pham <beverly.pham@gmail.com>
2019-02-13 20:12:05 -0800

    make functions to create and insert into database instead

--- sqlite_db.py
+++ sqlite_db.py
@@ -1,9 +1,19 @@
 import sqlite3
 
 
-conn = sqlite3.connect("lite.db")
-cur = conn.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS something (item TEXT, quantity INTEGER, price REAL)')
-cur.execute('INSERT INTO something VALUES ("Wine Glass", 8, 10.5)')
-conn.commit()
-conn.close()
+def create_table():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS something (item TEXT, quantity INTEGER, price REAL)')
+    conn.commit()
+    conn.close()
+
+
+def insert(item, quantity, price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute('INSERT INTO something VALUES (?, ?, ?)', (item, quantity, price))
+    conn.commit()
+    conn.close()
+
+insert("Water Glass", 10, 5)

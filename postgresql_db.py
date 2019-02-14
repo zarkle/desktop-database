11a30477f550a9274fff59c64a30a7580b3a830f
Beverly Pham <beverly.pham@gmail.com>
2019-02-13 21:03:17 -0800

    view table function added, same as sqlite3

--- postgresql_db.py
+++ postgresql_db.py
@@ -15,3 +15,12 @@ def insert(item, quantity, price):
     cur.execute('INSERT INTO something VALUES(%s, %s, %s)', (item, quantity, price))
     conn.commit()
     conn.close()
+
+
+def view():
+    conn = psycopg2.connect('dbname="something" user="postgres" password="postgres123" host="localhost" port="5432"')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM something')
+    rows = cur.fetchall()
+    conn.close()
+    return rows

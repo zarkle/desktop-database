8901d97abd4b9cfa0885a1cf200ef4d409a970b4
Beverly Pham <beverly.pham@gmail.com>
2019-02-13 21:00:02 -0800

    insert function made with no sql injection

--- postgresql_db.py
+++ postgresql_db.py
@@ -7,3 +7,11 @@ def create_table():
     cur.execute('CREATE TABLE IF NOT EXISTS something (item TEXT, quantity INTEGER, price REAL)')
     conn.commit()
     conn.close()
+
+
+def insert(item, quantity, price):
+    conn = psycopg2.connect('dbname="something" user="postgres" password="postgres123" host="localhost" port="5432"')
+    cur = conn.cursor()
+    cur.execute('INSERT INTO something VALUES(%s, %s, %s)', (item, quantity, price))
+    conn.commit()
+    conn.close()

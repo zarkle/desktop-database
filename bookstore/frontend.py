af4a552bf70679a77c9c4075f26ed42fc2cb4d9e
Beverly Pham <beverly.pham@gmail.com>
2019-02-14 15:59:09 -0800

    add button widgets, frontend GUI complete

--- bookstore/frontend.py
+++ bookstore/frontend.py
@@ -42,4 +42,22 @@ sb.grid(row=2, column=2, rowspan=6)
 lb.configure(yscrollcommand=sb.set)
 sb.configure(command=lb.yview)
 
+b1 = Button(window, text='View All', width=12)
+b1.grid(row=2, column=3)
+
+b2 = Button(window, text='Search Books', width=12)
+b2.grid(row=3, column=3)
+
+b3 = Button(window, text='Add Book', width=12)
+b3.grid(row=4, column=3)
+
+b4 = Button(window, text='Update Book', width=12)
+b4.grid(row=5, column=3)
+
+b5 = Button(window, text='Delete Book', width=12)
+b5.grid(row=6, column=3)
+
+b6 = Button(window, text='Exit', width=12)
+b6.grid(row=7, column=3)
+
 window.mainloop()

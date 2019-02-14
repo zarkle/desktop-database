7ac349ac1038a71b6b202040e1d0676c8c951925
Beverly Pham <beverly.pham@gmail.com>
2019-02-13 16:43:31 -0800

    make convert function and output to Text widgets

--- multi_widget_gui.py
+++ multi_widget_gui.py
@@ -1,9 +1,18 @@
 from tkinter import *
 from tkinter.ttk import *
 
+def convert():
+  kg = int(entry_val.get())
+  gram = kg * 1000
+  pound = kg * 2.20462
+  ounce = kg * 35.274
+  t1.insert(END, gram)
+  t2.insert(END, pound)
+  t3.insert(END, ounce)
+
 window = Tk()
 
-button = Button(window, text="Convert")
+button = Button(window, text="Convert", command=convert)
 button.grid(row=0, column=2)
 
 entry_val = StringVar()
@@ -12,5 +21,14 @@ entry.grid(row=0, column=1)
 
 label = Label(window, text="Kg")
 label.grid(row=0, column=0)
+
+t1 = Text(window, height=1, width=20)
+t1.grid(row=1, column=0)
+
+t2 = Text(window, height=1, width=20)
+t2.grid(row=1, column=1)
+
+t3 = Text(window, height=1, width=20)
+t3.grid(row=1, column=2)
 
 window.mainloop()

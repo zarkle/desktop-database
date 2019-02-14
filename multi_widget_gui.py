0cb37244f7360c1e7df26e6f29026d66e264e0e6
Beverly Pham <beverly.pham@gmail.com>
2019-02-13 16:32:19 -0800

    change Text to Label widget

--- multi_widget_gui.py
+++ multi_widget_gui.py
@@ -1,10 +1,16 @@
 from tkinter import *
+from tkinter.ttk import *
 
 window = Tk()
 
 button = Button(window, text="Convert")
-button.grid(row=0, column=1)
+button.grid(row=0, column=2)
 
+entry_val = StringVar()
+entry = Entry(window, textvariable=entry_val)
+entry.grid(row=0, column=1)
 
+label = Label(window, text="Kg")
+label.grid(row=0, column=0)
 
 window.mainloop()

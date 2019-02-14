d63b24b67dacc570177be71884502bb6175deb39
Beverly Pham <beverly.pham@gmail.com>
2019-02-13 20:05:01 -0800

    insert a record into database

--- sqlite_db.py
+++ sqlite_db.py
@@ -4,5 +4,6 @@ import sqlite3
 conn = sqlite3.connect("lite.db")
 cur = conn.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS something (item TEXT, quantity INTEGER, price REAL)')
+cur.execute('INSERT INTO something VALUES ("Wine Glass", 8, 10.5)')
 conn.commit()
 conn.close()

d3010275fe8b18e7409883fe7c24765e654012f8
Beverly Pham <beverly.pham@gmail.com>
2019-02-14 20:11:48 -0800

    delete button deletes record from database and displays updated records

--- bookstore/frontend.py
+++ bookstore/frontend.py
@@ -25,6 +25,19 @@ def add_command():
     lb.delete(0, END)
     lb.insert(END, (title.get(), author.get(), year.get(), isbn.get()))
 
+
+def get_selection(event):
+    """Get the book info for the current selected row."""
+    global selection
+    idx = lb.curselection()[0]
+    selection = lb.get(idx)
+
+
+def delete_command():
+    """Delete a book command."""
+    backend.delete(selection[0])
+    view_command()
+
 window = Tk()
 
 l1 = Label(window, text='Title')
@@ -64,6 +77,8 @@ sb.grid(row=2, column=2, rowspan=6)
 lb.configure(yscrollcommand=sb.set)
 sb.configure(command=lb.yview)
 
+lb.bind('<<ListboxSelect>>',get_selection)
+
 b1 = Button(window, text='View All', width=12, command=view_command)
 b1.grid(row=2, column=3)
 
@@ -76,7 +91,7 @@ b3.grid(row=4, column=3)
 b4 = Button(window, text='Update Book', width=12)
 b4.grid(row=5, column=3)
 
-b5 = Button(window, text='Delete Book', width=12)
+b5 = Button(window, text='Delete Book', width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
 b6 = Button(window, text='Exit', width=12)

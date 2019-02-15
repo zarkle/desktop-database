075d735a17b28201c597a2becf42d71ce5772832
Beverly Pham <beverly.pham@gmail.com>
2019-02-14 16:22:28 -0800

    make insert function to add book to database

--- bookstore/backend.py
+++ bookstore/backend.py
@@ -5,13 +5,23 @@ import sqlite3
 
 
 def connect_db():
+    """Connect to database."""
     conn = sqlite3.connect('books.db')
     cursor = conn.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS book
-                   (id INTEGER PRIMARY KEY, title TEXT,
+    cursor.execute('CREATE TABLE IF NOT EXISTS book \
+                   (id INTEGER PRIMARY KEY, title TEXT, \
                     author TEXT, year INTEGER, isbn INTEGER)')
     conn.commit()
     conn.close()
 
 
-connect()
+def insert(title, author, year, isbn):
+    """Insert a new book record."""
+    conn = sqlite3.connect('books.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO book VALUES (NULL,?,?,?,?)', (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+
+connect_db()
